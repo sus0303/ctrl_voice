bb0b9d08a1c008175a550e02025c1ac063a29802
sus0303 <susanahlopez3@gmail.com>
2026-04-23 10:48:34 -0500

    Change MQTT client ID from 'GIT-HUBC' to 'ABC'

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def on_message(client, userdata, message):
 
 broker="broker.mqttdashboard.com"
 port=1883
-client1= paho.Client("GIT-HUBC")
+client1= paho.Client("ABC")
 client1.on_message = on_message
 
 
